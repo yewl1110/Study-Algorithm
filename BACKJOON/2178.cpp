ce3cf0801487ad6ed1bbe9b8c74ea437fd3f82b7
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2019-11-04 15:10:07 +0900

    Create 2178.cpp

---
+++ BACKJOON/2178.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+#include <minmax.h>
+#include <algorithm>
+#pragma warning (disable:4996)
+using namespace std;
+
+typedef struct Point{
+	int x;
+	int y;
+};
+char arr[101][101];
+int _move[4][2] = {
+	//{0, -1}, {0, 1}, {-1, 0}, {1, 0} //UP DOWN LEFT RIGHT
+	{1,0},{0, 1},{-1,0},{0,-1}
+};
+int d[101][101];
+
+int x, y;
+
+int main() {
+	int N, M;
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		scanf("%s", arr[i]);
+	}
+
+
+	d[0][0] = 1;
+	queue<Point> q;
+	q.push({ 0,0 });
+	while (!q.empty()) {
+		Point p = q.front();
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			if (p.x >= 0 && p.x < N && p.y >= 0 && p.y < M) {
+				int x = _move[i][0];
+				int y = _move[i][1];
+				if (arr[p.x + x][p.y + y] == '1' && d[p.x + x][p.y + y] == 0) {
+					q.push({ p.x + x , p.y + y });
+					d[p.x + x][p.y + y] = d[p.x][p.y] + 1;
+				}
+			}
+		}
+	}
+	printf("%d\n", d[N - 1][M - 1]);
+}
